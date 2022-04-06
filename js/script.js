857315f67f7d2c85869bdbad8bf83370d6d35f05
rnyden <rnyden@hawk.iit.edu>
2022-04-05 19:33:57 -0500

    fixed indention

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function init(){
     alert("Reeanna Nyden: " + textIn);
     textOut.innerHTML = textIn
   }
-	var alertButton = document.getElementById("entrybutton");
+  var alertButton = document.getElementById("entrybutton");
   alertButton.addEventListener('click', alertPopUp);
 }
 window.addEventListener('load', init);
