dfd82d9a7360979330daa7285c7031861056584d
rnyden <rnyden@hawk.iit.edu>
2022-04-05 19:24:09 -0500

    added a pop up when the alert button is pressed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 function init(){
-
- 
-
-
-
-
-
-
+  function alertPopUp(){
+    var textIn = document.getElementById("entryinput").value;
+		alert("Reeanna Nyden: " + textIn);
+  }
+	var alertButton = document.getElementById("entrybutton");
+  alertButton.addEventListener('click', alertPopUp);
+}
 
 window.addEventListener('load', init);
-}
