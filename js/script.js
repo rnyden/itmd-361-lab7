e8ac00893fff98c3418cd86beb8e4293db5f89f8
rnyden <rnyden@hawk.iit.edu>
2022-04-05 19:36:50 -0500

    added semicolons to end of lines

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function init(){
   function alertPopUp(){
     var textIn = document.getElementById("entryinput").value;
-    var textOut = document.getElementById("textoutput")
+    var textOut = document.getElementById("textoutput");
     alert("Reeanna Nyden: " + textIn);
-    textOut.innerHTML = textIn
+    textOut.innerHTML = textIn;
   }
   var alertButton = document.getElementById("entrybutton");
   alertButton.addEventListener('click', alertPopUp);
